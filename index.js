4e04019814825aa52bf0a43050d53bdb0536e587
Nicole Beamer <tulip@Nicoles-iMac.home.sagemod>
2019-10-01 14:45:07 -0600

    still reviewing

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const hubsModel = require('./data/hubs-model.js');
 
 const server = express();
 
-//request handlers, takes the req that comes in and check for two things: the URL and the http method 
+//teach express how to read json from body this is middleware
+server.use(express.json());
 
+//request handlers, takes the req that comes in and check for two things: the URL and the http method
 server.get('/', (req, res) => {
     //order matters. the first arguement is the request the second arguement is the response
     res.send('hello asshole');
@@ -12,10 +14,24 @@ server.get('/', (req, res) => {
 
 server.get('/hubs', (req, res) => {
     //get a list of hubs from database and send the list of hubs back to client (using method from hubs-model.js)
-    hubsModel.find().then(hubs => {
+    hubsModel.find()
+    .then(hubs => {
         res.send(hubs);
     }).catch(error => {
         res.send(error);
+    });
+});
+
+server.post('/hubs', (req, res) => {
+    //get the hub data from the request 
+    const hubData = req.body;
+    //add hub to database
+    hubsModel.add(hubData)
+    .then(hub => {
+        res.json(hub);
+    })
+    .catch(error => {
+        res.json({ message: 'error saving the hub' });
     });
 });
 
